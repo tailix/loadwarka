aff8076b22d2b87498ffd878fe71b8bb2bae3c94
Alex Kotov <kotovalexarian@gmail.com>
2022-01-27 02:46:30 +0500

    Use C preprocessor

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -e
 BIN='../cross/root/bin'
 CROSS="$BIN/i386-elf-"
 
-${CROSS}as -c src/x86_stage1.S -o src/x86_stage1.o
-${CROSS}as -c src/x86_stage2.S -o src/x86_stage2.o
+${CROSS}gcc -c src/x86_stage1.S -o src/x86_stage1.o
+${CROSS}gcc -c src/x86_stage2.S -o src/x86_stage2.o
 ${CROSS}ld -Ttext 200000 --oformat binary -o src/x86_stage1.bin src/x86_stage1.o
 ${CROSS}ld -Ttext 200000 --oformat binary -o src/x86_stage2.bin src/x86_stage2.o
 ./testyboot mbr mbr.bin src/x86_stage1.bin
