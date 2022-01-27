61d542720e7a444b4585dfbbaeb55d901769037d
Alex Kotov <kotovalexarian@gmail.com>
2022-01-27 06:43:20 +0500

    Add linker scripts

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ CROSS="$BIN/i386-elf-"
 
 ${CROSS}gcc -c src/x86_stage1.S -o src/x86_stage1.o
 ${CROSS}gcc -c src/x86_stage2.S -o src/x86_stage2.o
-${CROSS}ld -Ttext 200000 --oformat binary -o src/x86_stage1.bin src/x86_stage1.o
-${CROSS}ld -Ttext 200000 --oformat binary -o src/x86_stage2.bin src/x86_stage2.o
+${CROSS}ld -Tsrc/x86_stage1.ld --oformat binary -o src/x86_stage1.bin src/x86_stage1.o
+${CROSS}ld -Tsrc/x86_stage2.ld --oformat binary -o src/x86_stage2.bin src/x86_stage2.o
 ./testyboot mbr mbr.bin src/x86_stage1.bin
 cat mbr.bin src/x86_stage2.bin > disk.img
