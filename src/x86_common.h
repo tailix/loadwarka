b0dd1e604bd52cd6b56528f4596adfd2efdd2d69
Alex Kotov <kotovalexarian@gmail.com>
2022-01-27 06:46:04 +0500

    Improve code

--- src/x86_common.h
+++
@@ -1,8 +0,0 @@
-#define STACK_BASE  0x2000
-#define STAGE1_BASE 0x7c00
-#define STAGE2_BASE 0x7e00
-
-#define DIST(x) (x - _start)
-
-#define STAGE1_ADDR(x) (DIST(x) + STAGE1_BASE)
-#define STAGE2_ADDR(x) (DIST(x) + STAGE2_BASE)
