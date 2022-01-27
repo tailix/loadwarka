e2e3234108b95d8af6d1d26653d2eda0be1b9337
Alex Kotov <kotovalexarian@gmail.com>
2022-01-27 23:21:06 +0500

    Improve build

--- build.sh
+++ build.sh
@@ -6,14 +6,20 @@ BIN='vendor/cross/root/bin'
 SRC='src/x86'
 CROSS="$BIN/i386-elf-"
 
+STAGE1_LDFLAGS='--defsym=program_start=0x7c00'
+STAGE2_LDFLAGS='--defsym=program_start=0x7e00'
+
+STAGE1_OBJS="$SRC/start.o $SRC/stage1.o"
+STAGE2_OBJS="$SRC/start.o $SRC/stage2.o"
+
 ./clean.sh
 
 ${CROSS}gcc -c $SRC/start.S  -o $SRC/start.o
 ${CROSS}gcc -c $SRC/stage1.S -o $SRC/stage1.o
 ${CROSS}gcc -c $SRC/stage2.S -o $SRC/stage2.o
 
-${CROSS}ld -T$SRC/linker.ld -o $SRC/stage1.bin --defsym=program_start=0x7c00 $SRC/start.o $SRC/stage1.o
-${CROSS}ld -T$SRC/linker.ld -o $SRC/stage2.bin --defsym=program_start=0x7e00 $SRC/start.o $SRC/stage2.o
+${CROSS}ld -T$SRC/linker.ld -o $SRC/stage1.bin $STAGE1_LDFLAGS $STAGE1_OBJS
+${CROSS}ld -T$SRC/linker.ld -o $SRC/stage2.bin $STAGE2_LDFLAGS $STAGE2_OBJS
 
 ./testyboot mbr mbr.bin $SRC/stage1.bin
 cat mbr.bin $SRC/stage2.bin > disk.img
