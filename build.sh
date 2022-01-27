7a53abea229e6eaf006cfdc2ed300e232800aa61
Alex Kotov <kotovalexarian@gmail.com>
2022-01-28 00:02:58 +0500

    Move hang func to separate module

--- build.sh
+++ build.sh
@@ -9,13 +9,13 @@ CROSS="$BIN/i386-elf-"
 STAGE1_LDFLAGS='--defsym=program_start=0x7c00'
 STAGE2_LDFLAGS='--defsym=program_start=0x7e00'
 
-STAGE1_OBJS="$SRC/header.o $SRC/print.o $SRC/stage1.o"
-STAGE2_OBJS="$SRC/header.o $SRC/print.o $SRC/stage2.o"
+STAGE1_OBJS="$SRC/header.o $SRC/common.o $SRC/stage1.o"
+STAGE2_OBJS="$SRC/header.o $SRC/common.o $SRC/stage2.o"
 
 ./clean.sh
 
 ${CROSS}gcc -c $SRC/header.S -o $SRC/header.o
-${CROSS}gcc -c $SRC/print.S  -o $SRC/print.o
+${CROSS}gcc -c $SRC/common.S -o $SRC/common.o
 ${CROSS}gcc -c $SRC/stage1.S -o $SRC/stage1.o
 ${CROSS}gcc -c $SRC/stage2.S -o $SRC/stage2.o
 
